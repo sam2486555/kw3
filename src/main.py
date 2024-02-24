9aeb4536fcdf9e5d734bc85a84cff78a08563686
Sergey Zamuraev <zamuraev210854@gmail.com>
2024-02-25 01:24:14 +0300

    add main.py выводит конечный результат 5 операций с датой, наименованием, счетами карты/получателя, суммой и валютой

---
+++ src/main.py
@@ -0,0 +1,15 @@
+from split import separation_line
+
+
+def output():
+    output_display = separation_line()
+
+    for transfer in output_display:
+        print(f"""
+{transfer['date'][8:10]+'.'+transfer['date'][5:7]+'.'+transfer['date'][:4]} {transfer['description']}
+{transfer.get('from', '')} -> {transfer['to']}
+{transfer['operationAmount']['amount']} {transfer['operationAmount']['currency']['name']}""")
+    return
+
+
+print(output())
